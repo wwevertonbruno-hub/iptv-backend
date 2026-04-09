f0e7f817c4544bb809b4f98e85ca15bb48489ce1
wwevertonbruno-hub <wwevertonbruno@gmail.com>
2026-04-08 23:32:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 
+// rota teste
 app.get("/", (req, res) => {
   res.send("Backend rodando 🚀");
 });
 
+// porta correta
 const PORT = process.env.PORT || 3000;
 
+// IMPORTANTE: 0.0.0.0
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor rodando na porta " + PORT);
 });
