67868eaf811d9b272ae9d8f4b4462e9fd965d928
wwevertonbruno-hub <wwevertonbruno@gmail.com>
2026-04-08 23:41:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
 const express = require("express");
+const fetch = require("node-fetch");
+
 const app = express();
 
-// rota teste
-app.get("/", (req, res) => {
-  res.send("Backend rodando 🚀");
+const BASE = "http://aptxu.com";
+const USER = "EzFgZ4v3w";
+const PASS = "vyesZtxZk";
+
+const headers = {
+  "User-Agent": "VLC/3.0.0 LibVLC",
+  "Referer": BASE,
+  "Accept": "*/*"
+};
+
+// canais
+app.get("/live", async (req, res) => {
+  const r = await fetch(`${BASE}/player_api.php?username=${USER}&password=${PASS}&action=get_live_streams`, { headers });
+  res.json(await r.json());
 });
 
-// porta correta
-const PORT = process.env.PORT || 3000;
+// imagens
+app.get("/img", async (req, res) => {
+  const r = await fetch(req.query.url, { headers });
+  res.set("Content-Type", r.headers.get("content-type"));
+  r.body.pipe(res);
+});
 
-// IMPORTANTE: 0.0.0.0
+// player
+app.get("/play", async (req, res) => {
+  const r = await fetch(req.query.url, { headers });
+  res.set("Content-Type", r.headers.get("content-type"));
+  r.body.pipe(res);
+});
+
+const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor rodando na porta " + PORT);
 });
