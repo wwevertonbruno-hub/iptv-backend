35c2eac873092c4530949177e83dfd0685b4cbf9
wwevertonbruno-hub <wwevertonbruno@gmail.com>
2026-04-11 21:53:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 // Headers IPTV (Bypass)
 const standardHeaders = {
-  "User-Agent": "IPTVSmartersPlayer",
+  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
   "Accept": "*/*",
   "Accept-Language": "en-US,en;q=0.9",
   "X-Requested-With": "com.nst.iptvsmartersbox",
